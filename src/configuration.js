0bdec4cccd2987b537f5231d8c7bd4afb9d7ba11
Eren <ceyhunerenerguven.07@outlook.com>
2023-01-17 13:41:49 +0300

    Fixes implemented.

--- src/configuration.js
+++ src/configuration.js
@@ -1,11 +1,28 @@
 module.exports = {
   Wyvern: {
-    latest: "1.0.2",
+    latest: "1.0.5",
     dev: null,
     
-    supported: ["1.0.2"],
-    unsupported: ["Beta Release 4", "Beta Release 3", "Beta Release 2", "Beta Release 1", "Alpha Release 5", "Alpha Release 4", "Alpha Release 3", "Alpha Release 2", "Alpha Release 1"],
-    deprecated: ["1.0.0"]
+    supported: [
+      "1.0.5"
+    ],
+    unsupported: [
+      "1.0.0",
+      "Beta Release 4",
+      "Beta Release 3",
+      "Beta Release 2",
+      "Beta Release 1",
+      "Alpha Release 5",
+      "Alpha Release 4",
+      "Alpha Release 3",
+      "Alpha Release 2",
+      "Alpha Release 1"
+    ],
+    deprecated: [
+      "1.0.2"
+    ],
+
+    script: "oEO&z>(3M8G@J(X0Jm8i'U3MyK^G$h2hQd@LJqZY<&Z6$^>lB9oQEy5@7oe=)qynLYqvLwPaRf7x1NRdCFkNDWLbDd@R3n^w{aRl)6|{k$S'6='QkO$Mum3mSerPu(Z1=4Qm'egqjKG)Y7v29T'VQ0wu<T1|f'P&4MdCXC{Mf0qEvYD1DA}7EDL1WFK3|\"VoDRM=j?yhVBumU{X3WO0nQ6rEP21R8Ya|}HeDmuG{>?14FZNLgV?nT=EXZyy=N%Ot"
   },
 
   Dolphin: {
@@ -14,6 +31,8 @@ module.exports = {
 
     supported: [],
     unsupported: [],
-    deprecated: []
+    deprecated: [],
+
+    script: "UqVTnbOvmqTmyS)OP$G?9QyChtZskwYg&N<tk)AdirVYj)myb51S0T|NVQ\"wo|LnvYJ4=GSO{rVWK^bgxYmEdA&sJSAezjO^ISyhK}5vHJR'(tXFQ$V4grrti2^SICs8^fzC'0Hc5}EAL>YDMdRM\"x38c3)&@)<YAX&M|f2=0ejZUZ<JBEsTiAHb^D?DH$_YiPCgbOR>3xyO{G1FLGK%V1z@RlXv^bKA>Qr{%0%=2apWLVqRphkjBNCq1hIZ_Ixa"
   }
 };
--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
-const port = 3000;
-
-const wss = new (require("ws")).Server({ port });
+const app = (require("express"))();
+const wss = new (require("ws")).Server({ server: app });
 
 const configuration = require("./configuration");
 
 wss.on("connection", (socket) => {
-  console.log(`[SocketServer] New socket connected.`);
+  console.log(`[Server] New socket connected.`);
 
   socket.send(JSON.stringify(configuration));
 });
+
+app.on("upgrade", (request, socket, head) => {
+  wss.emit("upgrade", request, socket);
+});
+
+app.get("/", (req, res) => {
+  res.statusCode = 200;
+
+  res.send("WeeApi");
+});
+
+app.listen(3000);
 
 // require("./test-connection");
